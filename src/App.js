9a60e673c216eb732f845e948aed783337005fe1
Kelly <kellyliang95@gmail.com>
2019-03-24 13:01:56 -0400

    navlink created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import { NavBar } from '../src/components/navbar/navbar';
+import { Home } from '../src/components/home/home';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <NavBar />
+       <main>
+         <Switch>
+           <Route path="/" component={Home}/>
+           <Route path="/movies" />
+           <Route path="/people" />
+           <Route path="/locations" />
+         </Switch>
+       </main>
       </div>
     );
   }
---
+++ src/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home = () => {
+  return(
+    <div className="home">
+      <h1>Welcome to GhibliApp</h1>
+    </div>
+  )
+}; 
+
